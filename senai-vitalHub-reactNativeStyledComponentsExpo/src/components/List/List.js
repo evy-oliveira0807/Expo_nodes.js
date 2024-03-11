fe4d84d760fdd260dbfdfff935c67892fbc73332
Evelyn Oliveira Ribeiro <53143873876@info.local>
2024-03-11 10:52:26 -0300

    .

--- senai-vitalHub-reactNativeStyledComponentsExpo/src/components/List/List.js
+++
@@ -1,8 +0,0 @@
-import { FlatList } from "react-native-web";
-import styled from "styled-components";
-
-export const ListComponent = styled(FlatList)`
-   width: 100%;
-   background-color: yellow;
-   margin-top: 20px;
-`
--- senai-vitalHub-reactNativeStyledComponentsExpo/src/screens/Home/style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const FilterAppointment = styled.View`
- width: 90%;
- flex-Direction: row;
- justify-content: space-between;
- margin-top: 38px;
-`
